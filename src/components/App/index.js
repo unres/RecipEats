7188ad6917b2945df8f42c8b33e90cdec4191c2d
unres <timothy.bell@ttu.edu>
2020-02-27 18:42:16 -0600

    Finish Basic Router functionality

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { 
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+
+import Navigation from '../Navigation';
+import LandingPage from '../Landing';
+import PasswordForgetPage from '../PasswordForget';
+import HomePage from '../Home';
+
+import * as ROUTES from '../../constants/routes';
+
 const App = () => (
-  <div>
-    <h1>App</h1>
-  </div>
+  <Router>
+    <div>
+      <Navigation />
+      <hr />
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+      <Route path={ROUTES.HOME} component={HomePage} />
+    </div>
+  </Router>
 );
 export default App;
