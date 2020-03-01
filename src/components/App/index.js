1a7cdc9af1dcd7e6cf49de07e40813a41e274d0d
Timothy Bell <timothy.bell@ttu.edu>
2020-03-01 01:00:01 -0600

    Added Session handling

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { 
   BrowserRouter as Router,
   Route,
@@ -14,20 +14,48 @@ import RecipePage from '../Recipe';
 import CookbookPage from '../Cookbook';
 
 import * as ROUTES from '../../constants/routes';
+import { withFirebase } from '../Firebase';
 
-const App = () => (
-  <Router>
-    <div>
-      <Navigation />
-      <hr />
-      <Route exact path={ROUTES.LANDING} component={LandingPage} />
-      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-      <Route path={ROUTES.HOME} component={HomePage} />
-      <Route path={ROUTES.RECIPE} component={RecipePage} />
-      <Route path={ROUTES.COOKBOOK} component={CookbookPage} />
-    </div>
-  </Router>
-);
-export default App;
+class App extends Component {
+  constructor(props) {
+    super (props);
+
+    this.state = {
+      authUser: null,
+    };
+  }
+  
+  componentDidMount() {
+    this.listener = this.props.firebase.auth.onAuthStateChanged(
+      authUser => {
+        authUser
+          ? this.setState({ authUser })
+          : this.setState({ authUser: null });
+      },
+    );
+  }
+
+  componentWillUnmount() {
+    this.listener();
+  }
+
+  render() {
+    return (
+      <Router>
+        <Navigation authUser={this.state.authUser} />
+
+        <hr />
+
+        <Route exact path={ROUTES.LANDING} component={LandingPage} />
+        <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+        <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+        <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+        <Route path={ROUTES.HOME} component={HomePage} />
+        <Route path={ROUTES.RECIPE} component={RecipePage} />
+        <Route path={ROUTES.COOKBOOK} component={CookbookPage} />
+      </Router>
+    );
+  }
+}
+
+export default withFirebase(App);
