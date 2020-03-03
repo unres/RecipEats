604a9f55df8dc435f66450deaa9fe58d35e9ddb8
Stanley Yoang <stanleyyoang@gmail.com>
2020-03-03 14:25:11 -0600

    added form to module

--- src/components/Recipe/index.js
+++ src/components/Recipe/index.js
@@ -1,47 +1,48 @@
 import React from 'react';
-import Modal from "../Modal";
-import { Button } from 'semantic-ui-react';
+import { Button, Modal, Image, Checkbox, Form } from 'semantic-ui-react';
 
 const INITIAL_STATE = {
-  show: false,
-  msg: "Show Modal"
-};
+  title: '',
+  description: '',
+  portionSize: 1,
+  ingredients: [''],
+  instructions: [''],
+  public: false,
+  userID: 1,
+  collaborators: [1]
+}
 
 class Recipe extends React.Component{
-  constructor(props) {
-    super(props);
-
-    this.state = { ...INITIAL_STATE};
-  }
-
-  showModal = event => {
-    this.setState({
-      show: !this.state.show
-    });
-    if (this.state.show === false)
-      this.setState({
-        msg: "Hide Modal"
-      });
-    else
-      this.setState({
-        msg: "Show Modal"
-      });
-  };
-
   render() {
     return (
-      <div className="recipe">
-        <h1>Recipe</h1>
-        <Button onClick={e => {
-          this.showModal();
-        }}
-        > 
-          {this.state.msg}
-        </Button>
-        <Modal onClose={this.showModal} show={this.state.show}>
-          peepeepoopoo
-        </Modal>
-      </div>
+      <Modal trigger={<Button>Create a Recipe</Button>}>
+        <Modal.Header>Create a Recipe</Modal.Header>
+        <Modal.Content image>
+          <Image wrapped size='medium' src='https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png' />
+          <Modal.Description>
+            <Form>
+              <Form.Field>
+                <label>Title</label>
+                <input placeholder='Title' />
+              </Form.Field>
+              <Form.Field>
+                <label>Description</label>
+                <input placeholder='Description' />
+              </Form.Field>
+              <Form.Field>
+                <label>Portion Size</label>
+                <input placeholder='Portion Size' />
+              </Form.Field>
+              <Form.TextArea label='Ingredients' placeholder='Ingredients' />
+              <Form.TextArea label='Instructions' placeholder='Instructions' />
+              <Form.Field>
+                <Checkbox label='Public recipe' />
+              </Form.Field>
+              <Button type='submit'>Submit</Button>
+            </Form>
+          </Modal.Description>
+        </Modal.Content>
+      </Modal>
     );
   }
 }
