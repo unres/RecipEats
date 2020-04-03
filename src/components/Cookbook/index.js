d15104dc50e2663924a763fb5df5379b60b2c494
AlecMangkharakhili <alecmang097@gmail.com>
2020-04-02 21:46:50 -0500

    Began basic form for adding cookbooks

--- src/components/Cookbook/index.js
+++ src/components/Cookbook/index.js
@@ -1,10 +1,41 @@
 import React from 'react';
-
+import { Button, ModalHeader, Modal, ModalDescription, Form } from 'semantic-ui-react'
+import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
 import '../Cookbook/cookbook.css'
 
-const Cookbook = () => (
-  <div className='cookbook'>
-    <h1>Cookbook</h1>
-  </div>
-);
+const INITIAL_STATE = {
+  title: '',
+  description: '',
+  // Default public access to false
+  public: false,
+  userID: ''
+}
+
+class Cookbook extends React.Component{
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INITIAL_STATE };
+  }
+
+  render() {
+    return(
+      <div className='addCookBook'>
+        <Modal trigger={<Button size='massive'>Add New Cookbook</Button>}>
+          <ModalHeader>Create a Cookbook</ModalHeader>
+          <ModalDescription>
+            <Form onSubmit={console.log(this.state)}>
+              <Form.Input name='title' label='Title' placeholder='Title'></Form.Input>
+              <Form.Input name='description' label='Description' placeholder='Description'></Form.Input>
+              <Form.Radio label='Allow Public Access' toggle/>
+              <Button type='submit'>Submit</Button>
+            </Form>
+          </ModalDescription>
+        </Modal>
+      </div>
+    );
+  }
+}
+
 export default Cookbook;
