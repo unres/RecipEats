a368279bb5c81bc022c37396ba604d4b1646053f
AlecMangkharakhili <alecmang097@gmail.com>
2020-04-05 01:52:29 -0500

    Implemented basic submission

--- src/components/Cookbook/index.js
+++ src/components/Cookbook/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Button, ModalHeader, Modal, ModalDescription, Form } from 'semantic-ui-react'
+import { Button, ModalHeader, Modal, ModalDescription, Form } from 'semantic-ui-react';
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
 import '../Cookbook/cookbook.css'
 
+// Initial state for new cookbooks
 const INITIAL_STATE = {
   title: '',
   description: '',
@@ -15,8 +16,47 @@ const INITIAL_STATE = {
 class Cookbook extends React.Component{
   constructor(props) {
     super(props);
-
+    console.log(props)
+    // Assigns initial state to state variable for form to change and submit
+    // Affected by setState method
     this.state = { ...INITIAL_STATE };
+    this.writeToDB = this.writeToDB.bind(this);
+  }
+
+  writeToDB() {
+    // TODO: Figure out a way to generate cookbook ids (cid) for submission to database
+    // Using this.title just makes the cid undefined in the database
+    return this.props.firebase.cookbook(this.title)
+      .set({
+        ...this.state
+      })
+      .then(() => {
+        this.setState({ ...INITIAL_STATE });
+        this.props.history.push(ROUTES.COOKBOOK);
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+  }
+
+  // Function for form changes to change the values of the object's state in real time
+  // event.target.name must be the same name as the variable in the state
+  onChange = event => {
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+  // Handles state change for "Allow Public Access" radio button
+  toggleOnChange = event => {
+    if (this.state.public === false) {
+      this.setState({public: true});
+    }
+    else {
+      this.setState({public: false})
+    }
+  }
+
+  onSubmit = event => {
+    this.setState({ userID: this.props.firebase.getUID()}, this.writeToDB)
   }
 
   render() {
@@ -25,10 +65,10 @@ class Cookbook extends React.Component{
         <Modal trigger={<Button size='massive'>Add New Cookbook</Button>}>
           <ModalHeader>Create a Cookbook</ModalHeader>
           <ModalDescription>
-            <Form onSubmit={console.log(this.state)}>
-              <Form.Input name='title' label='Title' placeholder='Title'></Form.Input>
-              <Form.Input name='description' label='Description' placeholder='Description'></Form.Input>
-              <Form.Radio label='Allow Public Access' toggle/>
+            <Form onSubmit={this.onSubmit}>
+              <Form.Input name='title' label='Title' placeholder='Title' onChange={this.onChange}></Form.Input>
+              <Form.Input name='description' label='Description' placeholder='Description' onChange={this.onChange}></Form.Input>
+              <Form.Radio name='public' label='Allow Public Access' onChange={this.toggleOnChange} toggle/>
               <Button type='submit'>Submit</Button>
             </Form>
           </ModalDescription>
@@ -38,4 +78,4 @@ class Cookbook extends React.Component{
   }
 }
 
-export default Cookbook;
+export default withFirebase(Cookbook);
