dd46616cd0b03cc396962bff7fed53a45db4026a
Timothy Bell <timothy.bell@ttu.edu>
2020-04-06 16:09:25 -0500

    Merging into Master from AccountPage (#8)
    * Update index.js
    
    Added password change to account page
    
    * Added title of change password
    
    Co-authored-by: Brigham Muench <brigham.muench@ttu.edu>

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
-import '../Account/account.css'
-
-const Account = () => (
+import PasswordChangeForm from '../PasswordChange';
+import "../Account/account.css"
+const AccountPage = () => (
   <div className='account'>
-    <h1>Account</h1>
+    <h1>Account Page</h1>
+    <h2>Change Password</h2>
+    <PasswordChangeForm />
   </div>
 );
-export default Account;
+export default AccountPage;
