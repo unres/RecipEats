2fa73599570d685f50e2bbb2a466e4678859339b
unres <timothy.bell@ttu.edu>
2020-04-06 16:06:32 -0500

    Added title of change password

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -4,6 +4,7 @@ import "../Account/account.css"
 const AccountPage = () => (
   <div className='account'>
     <h1>Account Page</h1>
+    <h2>Change Password</h2>
     <PasswordChangeForm />
   </div>
 );
