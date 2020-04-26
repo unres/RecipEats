df6fc05caff05faca49e2eb12b74532161e2fc3b
Brigham Muench <brigham.muench@ttu.edu>
2020-04-25 20:49:44 -0500

    Update index.js

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import PasswordChangeForm from '../PasswordChange';
 import "../Account/account.css"
-const AccountPage = () => (
+class AccountPage extends React.Component{
+
+render(){
+  return(
   <div className='account'>
     <h1>Account Page</h1>
     <h2>Change Password</h2>
     <PasswordChangeForm />
   </div>
-);
+  )
+}
+
+
+}
 export default AccountPage;
