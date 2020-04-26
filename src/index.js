1c91eea7c9feb284279ec753d88179d5faf10dec
Nikki Green <angela.nikki.green@ttu.edu>
2020-04-26 16:24:30 -0500

    Import Semantic.Less to be used in the website

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import Firebase, { FirebaseContext } from "./components/Firebase";
 
+import 'semantic-ui-less/semantic.less';
+
 ReactDOM.render(
     <FirebaseContext.Provider value={new Firebase()} >
         <App />
