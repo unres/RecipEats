a662cf685deaf667f150e5223b5e6accefd87541
Timothy Bell <timothy.bell@ttu.edu>
2020-05-01 19:21:36 -0500

    Likes finished for discover page

--- src/components/RecipeCard/likes.js
+++ src/components/RecipeCard/likes.js
@@ -6,7 +6,7 @@ import { Icon, Button } from 'semantic-ui-react';
 class Likes extends Component {
     constructor(props){
         super(props);
-        this.state = { ...this.props.recipe}
+        this.state = { ...this.props.recipe }
 
         this.writeToDB = this.writeToDB.bind(this);
     }
@@ -15,20 +15,28 @@ class Likes extends Component {
         return this.props.firebase.recipe(this.props.recipe.rid)
         .set({
           ...this.state
-        }).then(console.log(this.state.likes))
+        })
         .catch(error => {
           this.setState({ error });
         });
     }
 
     onClick = () => { 
-        this.setState({ likes: this.props.recipe.likes + 1}, () => {this.writeToDB()});
+        this.setState((prevState) => ({ active: !prevState.active }), () => {     
+            if(this.state.active === true){
+                this.setState({ likes: this.props.recipe.likes + 1}, () => {this.writeToDB()});
+            }
+            else{
+                this.setState({ likes: this.props.recipe.likes - 1}, () => {this.writeToDB()});
+            }
+        })
     }
 
     render(){
+        const { active } = this.state
        return(
         <div>
-        <Button icon toggle={true} onClick={this.onClick}>
+        <Button icon toggle active={active} onClick={this.onClick}>
             <Icon name="heart" disabled />
             {this.props.recipe.likes}
         </Button>
